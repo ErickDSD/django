5df93c0dbad8ac7586cc7550d05cbe6bdacd819d
ersanchez <e120300168@gmail.com>
2019-05-04 18:32:13 -0500

    iihpome

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+
+
 urlpatterns = [
-    url(r'^', include(iuafhome.urls)),
+    url(r'^', include('iuafhome.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
