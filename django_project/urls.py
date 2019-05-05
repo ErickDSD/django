2c2e66d2a586fd18642a23278a8a6d987e5a2b93
ersanchez <e120300168@gmail.com>
2019-05-05 01:00:03 -0500

    media

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     url(r'^', include('iuafhome.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # if settings.DEBUG:
 #     from django.conf.urls.static import static
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
