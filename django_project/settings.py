e0f012b558a6316478a9da69b50f9417b7e00d5f
ersanchez <e120300168@gmail.com>
2019-05-05 01:02:35 -0500

    media

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
 MEDIA_ROOT = os.path.join(BASE_DIR,'static',)
-print('media:',MEDIA_ROOT)
+
 # STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)),'static')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # Allow Django from all hosts. This snippet is installed from
--- django_project/urls.py
+++ django_project/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
 ]
 #static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# if settings.DEBUG:
-#     from django.conf.urls.static import static
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ if settings.DEBUG:
+     from django.conf.urls.static import static
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
