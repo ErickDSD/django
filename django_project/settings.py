8c920f33b00fcd62c1871900594dd6cd19ea0b6a
ErickDSD <e120300168@gmail.com>
2019-05-11 11:14:17 -0500

    static

--- django_project/settings.py
+++ django_project/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # if not DEBUG: 
 #     STATIC_ROOT = '/home/django/http://68.183.170.168/static/'
 
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 #   os.path.join(BASE_DIR, 'static'),
 # )
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # Allow Django from all hosts. This snippet is installed from
