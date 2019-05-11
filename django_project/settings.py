129eaa8fbddb614d4cb4464d7cdb44d028490227
ErickDSD <e120300168@gmail.com>
2019-05-11 10:43:10 -0500

    static

--- django_project/settings.py
+++ django_project/settings.py
@@ -117,9 +117,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # if not DEBUG: 
 #     STATIC_ROOT = '/home/django/http://68.183.170.168/static/'
 
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#   os.path.join(BASE_DIR, 'static'),
+# )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
