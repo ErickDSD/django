37943ae1706982f2fb49a966d025ea41b98ad6fa
ErickDSD <e120300168@gmail.com>
2019-05-11 11:57:06 -0500

    static

--- django_project/settings.py
+++ django_project/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # if not DEBUG: 
 #     STATIC_ROOT = '/home/django/http://68.183.170.168/static/'
 
